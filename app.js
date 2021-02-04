f1c26be71b04ed3d341d265f8c4e9f647be9b675
Steve Wood <steve@gowildly.com>
2021-02-04 09:55:47 -0800

    Adding bigger delay and attempting actual summary

--- app.js
+++ app.js
@@ -23,10 +23,11 @@ app.message('e', async ({ message, say }) => {
     // Set the idle timeout buffer again
     idleBuffer = setTimeout(() => {
         console.log("Dispatching request");
+        console.log(summary);
         axios.post(
             'https://api.openai.com/v1/engines/davinci/completions',
             {
-                "prompt": "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus.\n\nJupiter is primarily composed of hydrogen with a quarter of its mass being helium, though helium comprises only about a tenth of the number of molecules. It may also have a rocky core of heavier elements,[21] but like the other giant planets, Jupiter lacks a well-defined solid surface. Because of its rapid rotation, the planet's shape is that of an oblate spheroid (it has a slight but noticeable bulge around the equator).\n\nI rephrased this for my daughter, in plain language a second grader can understand:", 
+                "prompt": summary, 
                 "temperature": 0.3,
                 "max_tokens": 64,
                 "top_p": 1
@@ -43,7 +44,7 @@ app.message('e', async ({ message, say }) => {
                     completion = response.data.choices[0].text;
                 }
 
-                say(`Hey there <@${message.user}>! I think you're about to say: ${completion}`);
+                say(`Hey there <@${message.user}>! Let me summarize all of this great info for you:\r${completion}`);
                 idleBuffer = null;
                 summary = "";
                 counter = 0;
@@ -57,7 +58,7 @@ app.message('e', async ({ message, say }) => {
                 console.log(error);
             });
 
-    }, 1500);
+    }, 15000);
 });
 
 (async () => {
